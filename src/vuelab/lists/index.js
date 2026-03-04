61b7053853e3536d0bcfa07b48941746bf5823ac
Maksym Vovk <MaksemVovk@gmail.com>
2026-03-04 14:45:18 +0200

    add composable fucntion for lists (#15)

--- src/vuelab/lists/index.js
+++ src/vuelab/lists/index.js
@@ -1,5 +1,15 @@
 import countries from './countries.js'
 
-export default {
-  countries: countries,
+const lists = new Map([
+  ['countries', countries]
+])
+
+export const useLists = (type) => {
+  const keys = Array.from(lists.keys())
+
+  if (!keys.includes(type)) {
+    throw new Error(`List type "${type}" is not defined. Available types: ${keys.join(', ')}`)
+  }
+
+  return lists.get(type)
 }
